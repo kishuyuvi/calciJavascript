32c7f3be29bafdd0187a7be24ebf546bc5b2c65a
kishuyuvi <kishuyuvi@gmail.com>
2016-01-02 21:24:17 +0100

    commit

---
+++ script/script.js
@@ -0,0 +1,54 @@
+function click1(){
+    document.form.result.value += "1";
+}
+    function click2(){
+    document.form.result.value += "2";
+}
+    function click3(){
+    document.form.result.value += "3";
+}
+    function click4(){
+    document.form.result.value += "4";
+}
+    function click5(){
+    document.form.result.value += "5";
+}
+    function click6(){
+    document.form.result.value += "6";
+}
+    function click7(){
+    document.form.result.value += "7";
+}
+    function click8(){
+    document.form.result.value += "8";
+}
+    function click9(){
+    document.form.result.value += "9";
+}
+      function click0(){
+    document.form.result.value += "0";
+}
+    function clickdot(){
+    document.form.result.value += ".";
+}
+    
+    function clickplus(){
+    document.form.result.value += "+";
+}
+    function clickminus(){
+    document.form.result.value += "-";
+}
+    function clickmul(){
+    document.form.result.value += "*";
+}
+    function clickdiv(){
+    document.form.result.value += "/";
+}
+  function clr(){
+    document.form.result.value = "";
+}   
+    
+function equals(){
+    var eq=eval(document.form.result.value)
+    document.form.result.value=eq;
+}
